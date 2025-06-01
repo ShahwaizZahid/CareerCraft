fad496e8f25d2bcc0dcf3cd2d1633b261a139ada
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-06-01 14:25:48 +0500

    refactor: usefetch hook for fetch the data from the db

---
+++ hooks/use-fetch.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { toast } from "sonner";
+
+type FetchCallback<Args extends unknown[], T> = (...args: Args) => Promise<T>;
+
+function useFetch<Args extends unknown[], T>(cb: FetchCallback<Args, T>) {
+  const [data, setData] = useState<T | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fn = async (...args: Args): Promise<void> => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);
+      setData(response);
+      setError(null);
+    } catch (err: unknown) {
+      const error = err instanceof Error ? err : new Error("Unknown error");
+      setError(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn, setData };
+}
+
+export default useFetch;
