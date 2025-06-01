51ebc0a77ce0ccf518a2e5efaec13ad78a8bc957
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-06-01 23:52:49 +0500

    create api function for inngest to update industry insight after a week

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -4,4 +4,9 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({
   id: "career-craft",
   name: "Career Caraft",
+  credentials: {
+    gemini: {
+      apiKey: process.env.GEMINI_API_KEY!,
+    },
+  },
 });
